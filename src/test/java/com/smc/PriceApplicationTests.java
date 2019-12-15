84fcd915a803a3bf35bd1ce5fb1e68026f2bd16a
xuexp <xuexp@cn.ibm.com>
2019-12-15 19:59:11 +0800

    test class

--- src/test/java/com/smc/PriceApplicationTests.java
+++ src/test/java/com/smc/PriceApplicationTests.java
@@ -1,12 +1,16 @@
 package com.smc;
 
-import com.smc.service.StockPriceService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+// import com.smc.service.StockPriceService;
+// import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import org.springframework.test.context.junit4.SpringRunner;
 
+// import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PriceApplicationTests {
 
